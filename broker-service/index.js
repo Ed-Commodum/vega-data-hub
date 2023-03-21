19d6fbf85f949c10b1107d8c4bf96ce19e795343
Ed <Edward@commodum.io>
2023-03-21 15:16:40 +0000

    .

--- broker-service/index.js
+++ broker-service/index.js
@@ -30,13 +30,19 @@ function startBrokerServer(nano, BusEvent) {
 
 async function main() {
 
+
+    const root1 = await protobuf.load('/app/events.proto');
+    console.log(root1);
+    const BusEvent = root.lookupType(vega.events.v1.BusEvent);
+
+
     // Load events proto and BusEvent type
     const root = new protobuf.Root();
     root.resolvePath = (origin, target) => {
         console.log(`origin: ${origin}, target: ${target}`);
         return rootPath + target;
     };
-    root.load(relativeEventsProtoPath).then((root) => { console.log(root) }).then((root) => { console.log(root.lookupType(vega.events.v1.BusEvent)) });
+    root.load(relativeEventsProtoPath).then((root) => { console.log(root); return root; }).then((root) => { console.log(root.lookupType(vega.events.v1.BusEvent)) });
     // const root = protobuf.loadSync({ root: rootPath, file: relativeEventsProtoPath });
     console.log(root);
     // const BusEvent = root.lookupType(vega.events.v1.BusEvent);
