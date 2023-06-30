41cc3aa41e0cbb963ff967c31766fc183d0ea422
ed <edward@commodum.io>
2023-06-30 09:04:11 +0000

    change consumer group IDs for testing

--- trades-service/index.js
+++ trades-service/index.js
@@ -929,7 +929,7 @@ const setConsumer = (kafkaClient, kafkaConsumer) => {
     //     console.log(evt);
     //     mostRecentBeginBlock = evt.beginBlock;
     // });
-    kafkaConsumer = new kafka.Consumer(kafkaClient, [], { groupId: "trades-group-12" });
+    kafkaConsumer = new kafka.Consumer(kafkaClient, [], { groupId: "trades-group-13" });
     kafkaConsumer.on("message", (msg) => {
         // console.log("New message");
         const evt = JSON.parse(msg.value);
